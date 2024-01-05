05076ddd39b3aa3e77e0fd4fa62d646323a175ec
madangel <madcali@hotmail.com>
2024-01-06 00:39:27 +0100

    Update collection.js

--- collection.js
+++ collection.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const toy = toys[toyId];
         const toyThumbnail = document.createElement('div');
         toyThumbnail.classList.add('toy-thumbnail');
-        thumbnail = 'https://firebasestorage.googleapis.com/v0/b/transformers-collection.appspot.com/o/toys/' + toy + '%2thumbnail.jpg?alt=media'
+        thumbnail = 'https://firebasestorage.googleapis.com/v0/b/transformers-collection.appspot.com/o/toys/' + toy + '%2thumbnail.jpg?alt=media';
         toyThumbnail.innerHTML = `
             <img src="${thumbnail}" alt="${thumbnail}">
             <p>${toy.name}</p>
