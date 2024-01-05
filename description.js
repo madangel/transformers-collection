9be200a49ca798870eedcbad3c875bff5d32c5e6
madangel <madcali@hotmail.com>
2024-01-05 19:33:18 +0100

    Update description.js

--- description.js
+++ description.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var toyPhotosElement = document.getElementById('toyPhotos');
     var pdfViewerElement = document.getElementById('pdfViewer');
     var pdfLinkElement = document.getElementById('pdfLink');
+    var pdfLinkElement = document.getElementById('pdfLink');
+    var pdfContainerElement = document.getElementById('pdfContainer');
+
+    if (pdfLinkElement && pdfContainerElement) {
+        pdfLinkElement.addEventListener('click', function() {
+            pdfContainerElement.style.display = (pdfContainerElement.style.display === 'none') ? 'block' : 'none';
+        });
+    }
 
     var toyId = new URLSearchParams(window.location.search).get('id');
     fetch('https://transformers-collection-default-rtdb.europe-west1.firebasedatabase.app/toys/' + toyId + '.json')
