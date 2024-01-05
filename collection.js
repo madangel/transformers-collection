738f2aec10def36e28bcfa4d7c87a99ae685f97a
madangel <madcali@hotmail.com>
2024-01-05 16:49:21 +0100

    Update collection.js

--- collection.js
+++ collection.js
@@ -1,19 +1,19 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const toyList = document.getElementById('toyList');
-    const response = await fetch('https://transformers-collection-default-rtdb.europe-west1.firebasedatabase.app/toys.json');
-    const toys = await response.json();
-
-    for (const toyId in toys) {
-        const toy = toys[toyId];
-        const toyThumbnail = document.createElement('div');
-        toyThumbnail.classList.add('toy-thumbnail');
-        toyThumbnail.innerHTML = `
-            <img src="${toy.photos[0]}" alt="${toy.name}">
-            <p>${toy.name}</p>
-        `;
-        toyThumbnail.addEventListener('click', () => {
-            window.location.href = `description.html?id=${toyId}`;
+document.addEventListener('DOMContentLoaded', function() {
+    var toyList = document.getElementById('toyList');
+    fetch('https://YOUR-FIREBASE-PROJECT-ID.firebaseio.com/toys.json')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(toys) {
+            toyList.innerHTML = toys.map(function(toy) {
+                return '<div class="toy-thumbnail" onclick="redirectToDescription(\'' + toy.id + '\')">' +
+                           '<img src="' + toy.photos[0] + '" alt="' + toy.name + '">' +
+                           '<p>' + toy.name + '</p>' +
+                         '</div>';
+            }).join('');
         });
-        toyList.appendChild(toyThumbnail);
-    }
-}); 
+});
+
+function redirectToDescription(toyId) {
+    window.location.href = 'description.html?id=' + toyId;
+}
