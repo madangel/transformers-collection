682592b6766a348655e929fd44c0de26d8ea965a
madangel <madcali@hotmail.com>
2024-08-25 17:52:50 +0200

    Update collection.js
    Add manual Icon

--- collection.js
+++ collection.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         toyThumbnail.classList.add('toy-thumbnail');
         url_firebasestorage = 'https://firebasestorage.googleapis.com/v0/b/transformers-collection.appspot.com/o/';
         thumbnail = url_firebasestorage+'toys%2F'+toyId+'%2F001.jpg?alt=media';
+        manualIcon = url_firebasestorage+'icons%2FManual.jpg?alt=media'
         completIcon = url_firebasestorage+'icons%2F'+toy.weapon+'.jpg?alt=media'
         weaponCompletIcon = url_firebasestorage+'icons%2F'+toy.complete+'.jpg?alt=media'
         allianceIcon = url_firebasestorage+'icons%2F'+toy.alliance+'.jpg?alt=media'
@@ -16,6 +17,15 @@ document.addEventListener('DOMContentLoaded', async () => {
             <p>${toy.name}</p> 
             <img src="${thumbnail}" alt="${toyId}/001.jpg">
             <div class="icons" >
+                var pdfUrl = url_firebasestorage+'toys%2F'+toyId+'%2Fnotice.pdf';
+                fetch(pdfUrl, { method: 'HEAD' })
+                    .then(function(response) {
+                        if (response.ok) {
+                            <img src="${manualIcon}" alt="Manual" class="icon" />
+                        } 
+                    })
+                    .catch(function() {
+                    });
                 <img src="${weaponCompletIcon}" alt="Weapon Complet" class="icon" />
                 <img src="${completIcon}" alt="Complet" class="icon" />
                 <img src="${allianceIcon}" alt="Alliance" class="icon" />
