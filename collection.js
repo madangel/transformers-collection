586f2b689528f123ffa32568b3bb153eaf00576c
madangel <madcali@hotmail.com>
2024-08-25 12:52:46 +0200

    Update collection.js
    Modify icons src

--- collection.js
+++ collection.js
@@ -9,8 +9,8 @@ document.addEventListener('DOMContentLoaded', async () => {
         toyThumbnail.classList.add('toy-thumbnail');
         url_firebasestorage = 'https://firebasestorage.googleapis.com/v0/b/transformers-collection.appspot.com/o/';
         thumbnail = url_firebasestorage+'toys%2F'+toyId+'%2F001.jpg?alt=media';
-        completIcon = url_firebasestorage+'icons%2F'+toy.alliance+'.jpg?alt=media'
-        weaponCompletIcon = url_firebasestorage+'icons%2F'+toy.alliance+'.jpg?alt=media'
+        completIcon = url_firebasestorage+'icons%2F'+toy.weapon+'.jpg?alt=media'
+        weaponCompletIcon = url_firebasestorage+'icons%2F'+toy.complete+'.jpg?alt=media'
         allianceIcon = url_firebasestorage+'icons%2F'+toy.alliance+'.jpg?alt=media'
         toyThumbnail.innerHTML = `
             <p>${toy.name}</p> 
