9afbc9c3fea8e2c27c98e6ca1ef0e46e4dd57fd9
mad <madcali@hotmail.com>
2024-10-06 11:26:20 +0200

    visibleToys

--- collection.js
+++ collection.js
@@ -16,29 +16,31 @@ document.addEventListener('DOMContentLoaded', async () => {
     }));
 
     // Variables to store the current search query, sort criteria, and filter value
-    let filteredToys = [];
     let searchQuery = '';
     let sortCriteria = '';
     let allianceFilter = 'all';
+
+    let filteredToys = [];
+    if (toys && typeof toys === 'object') {
+        filteredToys = Object.values(toys);
+    } else {
+        console.error("The toys object is not defined or is null:", toys);
+        // Handle the error by displaying a message or returning early
+        return;
+    }
     
     // Display toys on the page (keeping the original thumbnail styling)
     function displayToys(toys) {
         // Filter and sort toys based on the current values of search, sort, and filter options
-        if (toys && typeof toys === 'object') {
-            filteredToys = Object.values(toys);
-        } else {
-            console.error("The toys object is not defined or is null:", toys);
-            // Handle the error by displaying a message or returning early
-            return;
-        }
+        let visibleToys = filteredToys;
         if (searchQuery) {
-            filteredToys = filteredToys.filter(toy => toy.name.toLowerCase().includes(searchQuery.toLowerCase()));
+            visibleToys = visibleToys.filter(toy => toy.name.toLowerCase().includes(searchQuery.toLowerCase()));
         }
         if (allianceFilter !== 'all') {
-            filteredToys = filteredToys.filter(toy => toy.alliance === allianceFilter);
+            visibleToys = visibleToys.filter(toy => toy.alliance === allianceFilter);
         }
         if (sortCriteria) {
-            filteredToys.sort((a, b) => {
+            visibleToys.sort((a, b) => {
                 if (typeof a[sortCriteria] === 'string' && typeof b[sortCriteria] === 'string') {
                     return a[sortCriteria].localeCompare(b[sortCriteria]);
                 } else {
@@ -49,7 +51,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         // Display the filtered and sorted toys
         toyList.innerHTML = ''; // Clear existing list of toys
-        filteredToys.forEach(toy => {
+        visibleToys.forEach(toy => {
             const toyThumbnail = document.createElement('div');
             toyThumbnail.classList.add('toy-thumbnail');
             const url_firebasestorage = 'https://firebasestorage.googleapis.com/v0/b/transformers-collection.appspot.com/o/';
